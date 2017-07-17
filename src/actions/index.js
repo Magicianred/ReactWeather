01ee86401ee12a7111c83a913df1e0cb11f78da6
Marco Poletto <marco@shoot-the-moon.co.uk>
2017-07-17 15:40:01 +0100

    5-68

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '876eb3b1d40f5ed70ae777539ebe57ff';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
